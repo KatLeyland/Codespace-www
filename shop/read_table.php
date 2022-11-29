5ab7cfc8453a64eec2a1989d687883211408e594
KatLeyland <112875876+KatLeyland@users.noreply.github.com>
2022-11-29 12:46:57 +0000

    formatting

--- shop/read_table.php
+++ shop/read_table.php
@@ -3,7 +3,12 @@ session_start();
 
 # Redirect if not logged in.
 if ( !isset( $_SESSION[ 'user_id' ] ) ) { require ( 'login_tools.php' ) ; load() ; };
+while($row = mysqli_fetch_array($r, MYSQLI_ASSOC))
+{
+echo'<tr>
+<td>'.$row['id'].'</td>
+<td>'.$row['first_name'].'</td>
+<td>'.$row['last_name'].'</td> '; }
 
-echo"{[first_name]}"
 
 ?>

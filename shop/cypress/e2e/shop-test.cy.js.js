f5aae416b124759bca0a640b306f528588d660ad
KatLeyland <112875876+KatLeyland@users.noreply.github.com>
2022-11-30 10:02:12 +0000

    change file

---
+++ shop/cypress/e2e/shop-test.cy.js.js
@@ -0,0 +1,28 @@
+describe ('Tests contents exist on page', () => {
+    it('Loads page', () => {
+        cy.visit('http://127.0.0.1:5500/index.html');
+      })
+    
+})
+describe ('Tests registration', () => {
+    it('clicks through from home to the registration page', () => {
+    
+     })
+    it('Submits without any input', () => {
+    
+    })
+    it('inputs names then submits', () => {
+    
+    })
+    
+})
+describe ('Tests login', () => {
+    it('', () => {
+    
+      })
+})
+describe ('Tests purchasing', () => {
+    it('', () => {
+    
+    })
+})
